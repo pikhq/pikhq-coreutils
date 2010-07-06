ec422b876fee9baa27db81529c5eafc834081959
Josiah Worcester <josiahw@gmail.com>
2010-07-06 01:46:39 -0500

    Basename cleanup.

--- basename/basename.c
+++ basename/basename.c
@@ -1,41 +1,45 @@
+/* Basic basename util
+ * Copyright (c) 2010 Josiah Worcester <josiahw@gmail.com>
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 static int all_slashes(const char *string, const size_t string_len);
-static int any_slashes(const char *string, const size_t string_len);
+static char *remove_leading(char *string);
+static void remove_suffix(char *string, const char *suffix);
+static void remove_trailing(char *string, const size_t string_len);
 
 static void basename(char *string, const char *suffix)
 {
-  if(!strcmp("", string)) {
-    printf(".");
-    return;
-  }
-
   size_t string_len = strlen(string);
-  if(all_slashes(string, string_len)) {
-    printf("/");
+
+  if(!strcmp("", string)) {
+    printf(".\n");
     return;
   }
-  
-  // Strip off trailing slashes
-  for(int i = string_len - 1; i != 0 && string[i] == '/'; i--)
-    string[i] = 0;
 
-  // Strip off leading slashes
-  char *new_string = NULL;
-  while((new_string = strchr(string, '/')) != NULL)
-    string = new_string + 1;
-
-  // Strip off the suffix, if any.
-  if(strcmp(string, suffix)) {
-    char *suffix_end = strchr(suffix, 0);
-    char *string_end = strchr(string, 0);
-    while(suffix_end != suffix && string_end != string && *string_end-- == *suffix_end--);
-    if(suffix_end == suffix)
-      *string_end = 0;
+  if(all_slashes(string, string_len)) {
+    printf("/\n");
+    return;
   }
-  printf("%s", string);
+
+  remove_trailing(string, string_len);
+  string = remove_leading(string);
+  remove_suffix(string, suffix);
+  printf("%s\n", string);
 }
 
 static int all_slashes(const char *string, const size_t string_len)
@@ -46,12 +50,29 @@ static int all_slashes(const char *string, const size_t string_len)
   return 1;
 }
 
-static int any_slashes(const char *string, const size_t string_len)
+static char *remove_leading(char *string)
 {
-  for(int i = 0; i!= string_len; i++)
-    if(string[i] == '/')
-      return 1;
-  return 0;
+  char *tmp = NULL;
+  while((tmp = strchr(string, '/')) != NULL)
+    string = tmp + 1;
+  return string;
+}
+
+static void remove_trailing(char *string, const size_t string_len)
+{
+  for(int i = string_len - 1; i != 0 && string[i] == '/'; i--)
+    string[i] = 0;
+}
+
+static void remove_suffix(char *string, const char *suffix)
+{
+  if(strcmp(string, suffix)) {
+    char *suffix_end = strchr(suffix, 0);
+    char *string_end = strchr(string, 0);
+    while(suffix_end != suffix && string_end != string && *string_end-- == *suffix_end--);
+    if(suffix_end == suffix)
+      *string_end = 0;
+  }
 }
 
 int main(int argc, char **argv)
@@ -63,5 +84,4 @@ int main(int argc, char **argv)
     basename(argv[1], "");
   else
     basename(argv[1], argv[2]);
-  printf("\n");
 }
