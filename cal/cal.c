f92028d6a327e6376906e014d67d5842f4a7cd23
Ada Worcester <oss@ada.pikhq.com>
2019-11-23 16:49:45 -0700

    Update name

--- cal/cal.c
+++ cal/cal.c
@@ -1,5 +1,5 @@
 /* Basic cal util
- * Copyright (c) 2010 Josiah Worcester <josiahw@gmail.com>
+ * Copyright (c) 2010 Ada Worcester <oss@ada.pikhq.com>
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
